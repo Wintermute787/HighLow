e124cf20c4d33a2ee111712a12f417052aa9c571
Joe Barnes and Michael Larragueta <joe@>
2019-04-22 13:38:55 -0700

    Add everything. it works

---
+++ HIgherLower/Number/number.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace HIgherLower
+{
+    class number
+    {
+        private int Num;
+        public number(int num)
+        {
+            Num = num;
+        }
+        public int getNum(){
+            return Num;
+        }
+
+
+    }
+}
---
+++ HIgherLower/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HIgherLower
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Would you like to play?");
+
+            bool check = Console.ReadLine().ToLower() == "y";
+            number guess = new number(5000);
+            int x = 0;
+            int guessCheck = guess.getNum();
+            int highNum = 10000;
+            int lowNum = 0;
+            if(check)
+            {
+                for (int i = 0; x == 0; i++)
+                {
+                    
+                    Console.WriteLine("Is your number higher or lower than " + guessCheck + "? [Higher/Lower/Correct]");
+                    string ans = Console.ReadLine().ToLower();
+                   
+                    bool high = ans == "higher";
+                    bool low = ans == "lower";
+                    bool correct = ans == "correct";
+                    if (high)
+                    {
+                        if(lowNum <= guessCheck)
+                        {
+                            lowNum = guessCheck;
+                        };
+                        guessCheck = guessCheck + (highNum - lowNum)/2;
+                    }
+                    else if (low)
+                    {
+                        if(highNum >= guessCheck)
+                        {
+                            highNum = guessCheck;
+                        };
+                        guessCheck = guessCheck - (highNum - lowNum) / 2;
+                    }
+                    else if (correct)
+                    {
+                        x = 1;
+                        break;
+                    }
+                    else Console.WriteLine("Please enter a valid choice");
+                };
+
+            }
+        }
+    }
+}
